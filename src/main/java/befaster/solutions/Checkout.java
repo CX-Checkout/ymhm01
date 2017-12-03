768d39465618b48c5bf7b6f8c12eaeb911e30f6e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 12:01:40 +0000

    Sun Dec 03 12:01:40 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -1,23 +1,24 @@
 package befaster.solutions;
 
-import java.util.Map;
+import java.util.HashMap;
 
 public class Checkout {
+
+    private static HashMap<String, Integer> productList = initializeCatalogue();
+
+    private static HashMap initializeCatalogue() {
+        HashMap map = new HashMap<String, Integer>();
+        map.put("A", 50);
+        map.put("B", 30);
+        map.put("C", 20);
+        map.put("D", 15);
+        return map;
+    }
+
     public static Integer checkout(String skus) {
-        String itemSku = "B";
-        int itemPrice = 30;
-        Map productList = new <String, Integer>();
-
-        if (skus == itemSku) {
-
-            return itemPrice;
-        }
-        if (skus == "C")
-            return 20;
-        if (skus == "D")
-            return 15;
-        if (skus == "A")
-            return 50;
-        return -1;
+        Integer price = -1;
+        if (productList.containsKey(skus))
+            price = productList.get(skus);
+        return price;
     }
 }
