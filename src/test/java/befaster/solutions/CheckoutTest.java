12e7fd4d389dbff55f5a6223011d1b322511390f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 14:01:54 +0000

    Sun Dec 03 14:01:54 GMT 2017

--- src/test/java/befaster/solutions/CheckoutTest.java
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -12,51 +12,9 @@ import static org.hamcrest.Matchers.equalTo;
 public class CheckoutTest {
 
     @Test
-    @Parameters({ "A, 50", "B, 30", "C, 20", "D, 15" })
+    @Parameters({ "A, 50", "B, 30", "C, 20", "D, 15", "W, -1", "AB, 80" })
     public void checkout_return_item_A_price_for_item_A_in_basket(String item, int amount) {
         assertThat(Checkout.checkout(item), equalTo(amount));
-    }
-/*
-    @Test
-    public void checkout_return_item_A_price_for_item_A_in_basket() {
-        String itemA = "A";
-        Integer itemAPrice = 50;
-        assertThat(Checkout.checkout(itemA), equalTo(itemAPrice));
-    }
-
-    @Test
-    public void checkout_return_item_B_price_for_item_B_in_basket() {
-        String itemB = "B";
-        Integer itemBPrice = 30;
-        assertThat(Checkout.checkout(itemB), equalTo(itemBPrice));
-    }
-
-    @Test
-    public void checkout_return_item_C_price_for_item_C_in_basket() {
-        String itemC = "C";
-        Integer itemCPrice = 20;
-        assertThat(Checkout.checkout(itemC), equalTo(itemCPrice));
-    }
-
-    @Test
-    public void checkout_return_item_D_price_for_item_D_in_basket() {
-        String itemD = "D";
-        Integer itemDPrice = 15;
-        assertThat(Checkout.checkout(itemD), equalTo(itemDPrice));
-    }
-
-    @Test
-    public void checkout_return_minus_one_for_invalid_item_in_basket() {
-        String invalidItem = "W";
-        Integer invalidPrice = -1;
-        assertThat(Checkout.checkout(invalidItem), equalTo(invalidPrice));
-    }
-
-    @Test
-    public void checkout_return_80_price_for_item_AB_in_basket() {
-        String items = "AB";
-        Integer amount = 80;
-        assertThat(Checkout.checkout(items), equalTo(amount));
     }
 
     @Test
@@ -64,5 +22,5 @@ public class CheckoutTest {
         String items = "AAA";
         Integer amount = 130;
         assertThat(Checkout.checkout(items), equalTo(amount));
-    }*/
+    }
 }
