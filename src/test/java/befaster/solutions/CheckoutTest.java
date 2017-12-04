6c9e0455e186efce73f1e868792dc66385e88836
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 18:47:35 +0000

    Mon Dec 04 18:47:35 GMT 2017

--- src/test/java/befaster/solutions/CheckoutTest.java
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -23,85 +23,16 @@ public class CheckoutTest {
     }
 
     @Test
-    @Parameters({"AB, 80", "AAA, 130", "BB, 45",
+    @Parameters({"AB, 80", "AAA, 130", "BB, 45", "AAAAAAAAA, 380",
             "BBAAA, 175", "BBB, 75", "AAAA, 180", "AAAAA, 200"})
     public void checkout_return_total_amount_for_items_with_discounts_in_basket(String items, int amount) {
         assertThat(Checkout.checkout(items), equalTo(amount));
     }
 
     @Test
-    @Parameters({"EEB, 80"})
-    public void checkout_return_80_price_for_item_EEB_in_basket(String items, int amount) {
-        assertThat(Checkout.checkout(items), equalTo(amount));
-    }
-
-    @Test
-    public void checkout_return_70_price_for_item_EB_in_basket() {
-        String items = "EB";
-        Integer amount = 70;
-        assertThat(Checkout.checkout(items), equalTo(amount));
-    }
-
-    @Test
-    public void checkout_return_160_price_for_item_EEEEBB_in_basket() {
-        String items = "EEEEBB";
-        Integer amount = 160;
-        assertThat(Checkout.checkout(items), equalTo(amount));
-    }
-
-    @Test
-    public void checkout_return_120_price_for_item_EEEB_in_basket() {
-        String items = "EEEB";
-        Integer amount = 120;
-        assertThat(Checkout.checkout(items), equalTo(amount));
-    }
-
-    @Test
-    public void checkout_return_380_price_for_item_AAAAAAAAA_in_basket() {
-        String items = "AAAAAAAAA";
-        Integer amount = 380;
-        assertThat(Checkout.checkout(items), equalTo(amount));
-    }
-
-    @Test
-    public void checkout_return_230_price_for_item_BEBBBBEE_in_basket() {
-        String items = "BEBBBBEE";
-        Integer amount = 210;
-        assertThat(Checkout.checkout(items), equalTo(amount));
-    }
-
-    @Test
-    public void checkout_return_665_price_for_item_ABCDECBAABCABBAAAEEAA_in_basket() {
-        String items = "ABCDECBAABCABBAAAEEAA";
-        Integer amount = 665;
-        assertThat(Checkout.checkout(items), equalTo(amount));
-    }
-
-    @Test
-    public void checkout_return_20_price_for_item_FFF_in_basket() {
-        String items = "FFF";
-        Integer amount = 20;
-        assertThat(Checkout.checkout(items), equalTo(amount));
-    }
-
-    @Test
-    public void checkout_return_20_price_for_item_FF_in_basket() {
-        String items = "FF";
-        Integer amount = 20;
-        assertThat(Checkout.checkout(items), equalTo(amount));
-    }
-
-    @Test
-    public void checkout_return_40_price_for_item_FFFFFF_in_basket() {
-        String items = "FFFFFF";
-        Integer amount = 40;
-        assertThat(Checkout.checkout(items), equalTo(amount));
-    }
-
-    @Test
-    public void checkout_return_30_price_for_item_FFFF_in_basket() {
-        String items = "FFFF";
-        Integer amount = 30;
+    @Parameters({"EEB, 80", "EB, 70", "EEEEBB, 160", "EEEB, 120",
+            "BEBBBBEE, 210", "ABCDECBAABCABBAAAEEAA, 665", "FFF, 20", "FF, 20", "FFFFFF, 40", "FFFF, 30"})
+    public void checkout_return_total_amount_for_item_with_cross_products_discounts_in_basket(String items, int amount) {
         assertThat(Checkout.checkout(items), equalTo(amount));
     }
 
