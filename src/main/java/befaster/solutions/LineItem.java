6b4fde97a5907e425b922114057188865fe962a0
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 19:26:35 +0000

    Mon Dec 04 19:26:35 GMT 2017

--- src/main/java/befaster/solutions/LineItem.java
+++ src/main/java/befaster/solutions/LineItem.java
@@ -22,6 +22,8 @@ public class LineItem {
         totalBasketLineAmount = applyDiscount(totalBasketLineAmount, (quantity) -> (quantity >= 2), 2, 'B', 45);
         totalBasketLineAmount = applyDiscount(totalBasketLineAmount, (quantity) -> (quantity >= 5), 5, 'P', 200);
         totalBasketLineAmount = applyDiscount(totalBasketLineAmount, (quantity) -> (quantity >= 3), 3, 'Q', 80);
+        totalBasketLineAmount = applyDiscount(totalBasketLineAmount, (quantity) -> (quantity >= 3), 3, 'V', 130);
+        totalBasketLineAmount = applyDiscount(totalBasketLineAmount, (quantity) -> (quantity >= 2), 2, 'V', 90);
 
         return totalBasketLineAmount;
     }
